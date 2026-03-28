99df48d375014f0e48959aaf506cf31a92f59456
Hitesh Kumar Sharma <hkshitesh@gmail.com>
2026-03-28 20:10:39 +0530

    calc project started

---
+++ src/main/java/devopssl/MyCalc.java
@@ -0,0 +1,23 @@
+package devopssl;
+
+public class MyCalc {
+	
+	int sum(int a,int b)
+	{
+		return a+b;
+	}
+	int diff(int a,int b)
+	{
+		return a-b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MyCalc calc = new MyCalc();
+		System.out.println("Sum is "+calc.sum(20, 10));
+		System.out.println("Diff is "+calc.diff(20, 10));
+
+	}
+
+}
